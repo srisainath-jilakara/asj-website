185363e74ac2da89cf4dfaf548985fdae9f8436d
srisainath-jilakara <50734704+srisainath-jilakara@users.noreply.github.com>
2026-02-04 00:21:26 +0530

    Enhance openTab function with smooth scrolling
    Added smooth scrolling to the active tab functionality.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 function openTab(tabId){
-const tabs=document.querySelectorAll('.tab');
-tabs.forEach(tab=>tab.classList.remove('active'));
 
-document.getElementById(tabId).classList.add('active');
+let tabs = document.querySelectorAll(".tab");
+
+tabs.forEach(tab => {
+tab.classList.remove("active");
+});
+
+document.getElementById(tabId).classList.add("active");
+
+window.scrollTo({
+top: document.querySelector(".container").offsetTop - 60,
+behavior: "smooth"
+});
+
 }
