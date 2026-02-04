7f774b19385a10ac053b5c3fe3f5f421a9147ccb
srisainath-jilakara <50734704+srisainath-jilakara@users.noreply.github.com>
2026-02-04 19:38:34 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
-function openTab(tabId) {
-  document.querySelectorAll('.tab').forEach(tab => {
+function openTab(tabId){
+  document.querySelectorAll('.tab').forEach(tab=>{
     tab.classList.remove('active');
   });
-
   document.getElementById(tabId).classList.add('active');
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", ()=>{
   const form = document.getElementById("contactForm");
-
-  if (form) {
-    form.addEventListener("submit", function (e) {
+  if(form){
+    form.addEventListener("submit", function(e){
       e.preventDefault();
       document.getElementById("status").innerText = "Message sent successfully!";
       form.reset();
