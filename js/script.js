6469cad07561b8624723ad9db1cfc55a8c7f8ea2
srisainath-jilakara <50734704+srisainath-jilakara@users.noreply.github.com>
2026-02-04 19:06:00 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,19 @@
-function openTab(tabId){
+function openTab(tabId) {
+  document.querySelectorAll('.tab').forEach(tab => {
+    tab.classList.remove('active');
+  });
 
-document.querySelectorAll('.tab').forEach(tab=>{
-tab.classList.remove('active');
-});
-
-document.getElementById(tabId).classList.add('active');
-
+  document.getElementById(tabId).classList.add('active');
 }
 
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("contactForm");
 
-const form=document.getElementById("contactForm");
-
-if(form){
-form.addEventListener("submit", function(e){
-e.preventDefault();
-document.getElementById("status").innerText="Message sent successfully!";
-form.reset();
-});
-}
-
+  if (form) {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      document.getElementById("status").innerText = "Message sent successfully!";
+      form.reset();
+    });
+  }
 });
